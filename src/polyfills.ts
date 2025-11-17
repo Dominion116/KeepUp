d589dd6ac2512cafe22ce6939c6e7dc22481519b
Dominion <limbotech116@gmail.com>
2025-11-17 20:39:36 +0100

    feat: enhance Ethereum provider interface and improve global polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,10 +2,29 @@
 import { Buffer } from 'buffer';
 import process from 'process';
 
+// Ethereum provider type
+interface EthereumProvider {
+  isMetaMask?: boolean;
+  request: (args: { method: string; params?: unknown[] }) => Promise<unknown>;
+  on?: (eventName: string, handler: (...args: unknown[]) => void) => void;
+  removeListener?: (eventName: string, handler: (...args: unknown[]) => void) => void;
+  selectedAddress?: string | null;
+  chainId?: string;
+}
+
 declare global {
   interface Window {
-      ethereum?: any;
+    ethereum?: EthereumProvider;
+    Buffer: typeof Buffer;
+    process: typeof process;
   }
+  
+  // eslint-disable-next-line no-var
+  var Buffer: typeof Buffer;
+  // eslint-disable-next-line no-var
+  var process: typeof process;
+  // eslint-disable-next-line no-var
+  var global: typeof globalThis;
 }
 
 // Global Buffer polyfill
@@ -13,7 +32,6 @@ if (!window.Buffer) {
   window.Buffer = Buffer;
   window.process = process;
 }
-
 
 // Global polyfill
 if (typeof globalThis !== 'undefined') {
@@ -24,17 +42,17 @@ if (typeof globalThis !== 'undefined') {
   }
 }
 
-
-// Polyfill for toformat and big number libraries
+// Polyfill for BigInt (for older browsers)
 if (typeof BigInt === 'undefined') {
-    window.BigInt = Object.assign(
-      function(value: string | number | bigint | boolean) {
-        return parseInt(value as string, 10);
-      },
-      {
-        asIntN: (bits: number, int: bigint) => int,
-        asUintN: (bits: number, int: bigint) => int,
-      }
-    ) as unknown as BigIntConstructor;
-  }
-export default {}; 
+  window.BigInt = Object.assign(
+    function(value: string | number | bigint | boolean) {
+      return parseInt(value as string, 10);
+    },
+    {
+      asIntN: (bits: number, int: bigint) => int,
+      asUintN: (bits: number, int: bigint) => int,
+    }
+  ) as unknown as BigIntConstructor;
+}
+
+export default {};
